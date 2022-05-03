53e8ad153b504225d6257804f9b622d60db14a4b
Advya <petrov.achiever@gmail.com>
2022-05-03 19:25:25 +0300

    validation of stock parameter

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,7 +1,16 @@
 class StocksController < ApplicationController
   def search
-    @stock = Stock.new_lookup(params[:stock])
-
-    render 'users/my_portfolio'
+    if params[:stock].present?
+      @stock = Stock.new_lookup(params[:stock])
+      if @stock.nil?        
+        render 'users/my_portfolio'
+      else
+        flash[:alert] = 'Please enter a valid symbol'
+        redirect_to my_portfolio_path
+      end      
+    else
+      flash[:alert] = 'Please enter right symbol'
+      redirect_to my_portfolio_path
+    end
   end
 end
