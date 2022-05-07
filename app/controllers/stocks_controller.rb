c26e0f7a098e4484b8e503250238628f4d5be8bd
Advya <petrov.achiever@gmail.com>
2022-05-07 21:21:08 +0300

    workaroud for format.js not working properly

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,16 +1,31 @@
 class StocksController < ApplicationController
+
   def search
-    if params[:stock].present?
-      @stock = Stock.new_lookup(params[:stock])
-      if @stock.nil?        
-        render 'users/my_portfolio'
-      else
-        flash[:alert] = 'Please enter a valid symbol'
-        redirect_to my_portfolio_path
-      end      
+    if params[:stock].blank?
+      flash[:danger] = "Please enter something"
     else
-      flash[:alert] = 'Please enter right symbol'
-      redirect_to my_portfolio_path
+      @stock = Stock.new_lookup(params[:stock])
+      flash[:danger] = "Incorrect symbol" unless @stock
     end
+    
+    render 'users/my_portfolio'
+    
+    # redirect_to my_portfolio_path
+
+
+    # if params[:stock].present?
+    #   @stock = Stock.new_lookup(params[:stock])
+    #   if @stock
+    #     respond_to do |format|
+    #       format.js { render partial: 'users/result.js' }
+    #     end
+    #   else
+    #     flash[:alert] = 'Please enter a valid symbol'
+    #     redirect_to my_portfolio_path
+    #   end      
+    # else
+    #   flash[:alert] = 'Please enter right symbol'
+    #   redirect_to my_portfolio_path
+    # end
   end
 end
