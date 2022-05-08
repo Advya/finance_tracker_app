1960483cac0cf5c56622b31e0a20b3125e08b6aa
Advya <petrov.achiever@gmail.com>
2022-05-08 14:33:20 +0300

    cleanup

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -9,23 +9,5 @@ class StocksController < ApplicationController
     end
     
     render 'users/my_portfolio'
-    
-    # redirect_to my_portfolio_path
-
-
-    # if params[:stock].present?
-    #   @stock = Stock.new_lookup(params[:stock])
-    #   if @stock
-    #     respond_to do |format|
-    #       format.js { render partial: 'users/result.js' }
-    #     end
-    #   else
-    #     flash[:alert] = 'Please enter a valid symbol'
-    #     redirect_to my_portfolio_path
-    #   end      
-    # else
-    #   flash[:alert] = 'Please enter right symbol'
-    #   redirect_to my_portfolio_path
-    # end
   end
 end
